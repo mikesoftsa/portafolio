066499139436c86ccbda9704beb6a883dfd2ff05
Miguel Triana <mikesoft241184@gmail.com>
2017-11-07 17:13:30 -0500

    Agregamos imagenes y servicio de productos

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -27,7 +27,7 @@ export class InformacionService {
   public carga_sobre_nosotros(){
     this.http.get("https://paginaweb-fffec.firebaseio.com/equipo.json")
              .subscribe( data=>{
-               console.log(data.json());
+              //  console.log(data.json());
                this.equipo = data.json();
                this.cargada_sobre_nosotros = true;
              });
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class ProductosService {
+
+  productos:any[] = [];
+  cargando:boolean = true;
+
+  constructor( public http:Http ) {
+    this.cargar_productos();
+  }
+
+  public cargar_productos(){
+    // if ( this.productos.length === 0 ){
+    //
+    // }
+
+    this.cargando = true;
+
+    this.http.get('https://paginaweb-fffec.firebaseio.com/productos_idx.json')
+        .subscribe( res => {
+          console.log( res.json());
+          this.cargando = false;
+        });
+  }
+
+}
